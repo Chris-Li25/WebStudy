42a8438d622aa7e58ce4cca419d0ee5666330106
chris <saber.li233@gmail.com>
2022-01-24 14:20:39 +0800

    update

--- Webpack/bilibili/38_merge_dev/src/App.jsx
+++ Webpack/bilibili/38_merge_dev/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter, Routes, Link, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
 import About from "./components/About.jsx";
 import Home from "./components/Home.jsx";
 
@@ -15,14 +15,14 @@ class App extends Component {
     return (
       <div>
         <h2>{this.state.title}</h2>
-        <HashRouter>
+        <BrowserRouter>
           <Link to="/home">首页</Link>
           <Link to="/about">关于</Link>
           <Routes>
             <Route path="/home" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </div>
     );
   }
